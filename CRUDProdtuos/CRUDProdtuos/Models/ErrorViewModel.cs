a22164deebbdb78a7074da0d10d11885850757d4
CodeMaik <maiconsilva1205@gmail.com>
2025-01-22 21:49:34 -0300

    adicionando o arquivo do projeto no repositório

---
+++ CRUDProdtuos/CRUDProdtuos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDProdtuos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
